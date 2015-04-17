acbd53203c62888d1485104aa56226ae8290ff55
tkempf <tkempf@hueper.de>
2015-04-17 15:30:20 +0200

    Tauber Tariftemplate geändert

--- media/breezingforms/tarifrechner/tauber.php
+++ media/breezingforms/tarifrechner/tauber.php
@@ -2,14 +2,16 @@
 // tarifradiodata Tauberenergie hat folgende Elemente
 // checked;gesamtpreis;tarifid;tauber;tarifanzeigename;tarifinfo;Endpreisbeschreibung;Arbeitspreis mit Beschreibung;ArbeitspreisNT mit Beschreibung;Grundpreis mit Beschreibung; 
 $tchecked=($iEx[0] == 1);
-$tpreis=htmlentities(trim($iEx[1]), ENT_QUOTES, 'UTF-8');$tpreisbeschr='EUR/Jahr';
+$tpreis=htmlentities(trim($iEx[1]), ENT_QUOTES, 'UTF-8');
+$tpreisbeschr='EUR/Jahr';
 $tid='tarif'.htmlentities(trim($iEx[2]), ENT_QUOTES, 'UTF-8');
+// $iEx[3] enthält den Selektor für das Template
 $tname=htmlentities(trim($iEx[4]), ENT_QUOTES, 'UTF-8');
 $tinfo=trim($iEx[5]);
 $tarbeitspreis='Verbrauchspreis HT: '.htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
-$tarbeitnt=($iEx[8] == 0) ? '' : 'Verbrauchspreis NT: '.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
+$tarbeitspreisnt=($iEx[7] == 0) ? '' : 'Verbrauchspreis NT: '.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
 $tgrund='Grundpreis: '.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').' EUR pro Jahr';
-$ttinhalt='<div class=\"bfToolTipLabel\"><b>Ihre Preisbestandteile ...</b><div/>'.$tarbeit.$tarbeitnt.$tgrund;
+$ttinhalt='<div class=\"bfToolTipLabel\"><b>Ihre Preisbestandteile ...</b><div/>'.$tarbeitspreis.$tarbeitspreisnt.$tgrund;
 echo '<div class="whTarif">'."\n";
 echo '   <div class="whTitel">'.$tname.'</div>';
 echo '   <div id="whPreis" class="whPreis"><span id="whPreisbestandteile'.$i.'" class="bfTooltip">'.$tpreis.'</span></div>'."\n";
