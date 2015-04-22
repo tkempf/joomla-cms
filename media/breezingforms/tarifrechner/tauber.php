451ace0faad50c062e9f42b07ef2f0709624967a
Kai Wiendlocha <kwiendlocha@hueper.de>
2015-04-22 14:02:13 +0200

    Stromtarifrechner-theme eingefügt Tarifrechnerergebnis  angepasst

--- media/breezingforms/tarifrechner/tauber.php
+++ media/breezingforms/tarifrechner/tauber.php
@@ -3,7 +3,7 @@
 // checked;gesamtpreis;tarifid;tauber;tarifanzeigename;tarifinfo;Endpreisbeschreibung;Arbeitspreis mit Beschreibung;ArbeitspreisNT mit Beschreibung;Grundpreis mit Beschreibung; 
 $tchecked=($iEx[0] == 1);
 $tpreis=htmlentities(trim($iEx[1]), ENT_QUOTES, 'UTF-8');
-$tpreisbeschr='EUR/Jahr';
+$tpreisbeschr=' EUR/Jahr';
 $tid='tarif'.htmlentities(trim($iEx[2]), ENT_QUOTES, 'UTF-8');
 // $iEx[3] enthält den Selektor für das Template
 $tname=htmlentities(trim($iEx[4]), ENT_QUOTES, 'UTF-8');
@@ -12,9 +12,10 @@ $tarbeitspreis='Verbrauchspreis HT: '.htmlentities(trim($iEx[6]), ENT_QUOTES, 'U
 $tarbeitspreisnt=($iEx[7] == 0) ? '' : 'Verbrauchspreis NT: '.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
 $tgrund='Grundpreis: '.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').' EUR pro Jahr';
 $ttinhalt='<div class=\"bfToolTipLabel\"><b>Ihre Preisbestandteile ...</b><div/>'.$tarbeitspreis.$tarbeitspreisnt.$tgrund;
+echo '<div class="whTarifWrapper">'."\n";
 echo '<div class="whTarif">'."\n";
-echo '   <div class="whTitel">'.$tname.'</div>';
-echo '   <div id="whPreis" class="whPreis"><span id="whPreisbestandteile'.$i.'" class="bfTooltip">'.$tpreis.'</span></div>'."\n";
+echo '   <p class="whTitel">'.$tname.'</p>';
+echo '   <div id="whPreis" class="whPreis">'.$tpreis.'<br>'.$tpreisbeschr.'<br><span id="whPreisbestandteile'.$i.'" class="bfTooltip"></span></div>'."\n";
 echo '
 		<script type="text/javascript"><!--
 			JQuery(document).ready(function() {
@@ -31,10 +32,12 @@ echo '
 				});
 			});
 		//--></script>';
-echo '   <div class="whPreisbeschreibung">'.$tpreisbeschr.'</div>'."\n";
-echo '   <div class="whSeparator"></div>';
-echo '   <div class="whText">'.$tinfo.'</div>'."\n";
+#echo '   <div class="whPreisbeschreibung">'.$tpreisbeschr.'</div>'."\n";
+#echo '   <div class="whSeparator"></div>';
+#echo '   <div class="whText">'.$tinfo.'</div>'."\n";
+echo '</div>'."\n";
 echo '   <div class="whRadio"><input '.($tchecked ? 'checked="checked" ' : '').' class="ff_elem" '.$tabIndex.$onclick.$onblur.$onchange.$onfocus.$onselect.$readonly.'type="radio" name="ff_nm_'.$mdata['bfName'].'[]" value="'.$tid.'" id="ff_elem'.$mdata['dbId'].$idExt.'"/>';
 echo '      '.$tname."</div>\n";
 echo '</div>'."\n";
+
 ?>
