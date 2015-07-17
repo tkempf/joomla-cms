50b2272525114f21eb74c40a422486f33b0c0a2c
tkempf <tkempf@hueper.de>
2015-07-17 14:27:59 +0200

    das Bootstrap Modal gedöns ist eingebaut, funktioniert aber noch nicht so wie es soll

--- media/breezingforms/tarifrechner/tauber.php
+++ media/breezingforms/tarifrechner/tauber.php
@@ -5,39 +5,46 @@ $tchecked=($iEx[0] == 1);
 $tpreis=htmlentities(trim($iEx[1]), ENT_QUOTES, 'UTF-8');
 $tpreisbeschr=' EUR/Jahr';
 $tid='tarif'.htmlentities(trim($iEx[2]), ENT_QUOTES, 'UTF-8');
-// $iEx[3] enthält den Selektor für das Template
+// $iEx[3] enthält	 den Selektor für das Template
 $tname=htmlentities(trim($iEx[4]), ENT_QUOTES, 'UTF-8');
 $tinfo=trim($iEx[5]);
 $tarbeitspreis='Verbrauchspreis HT: '.htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
 $tarbeitspreisnt=($iEx[7] == 0) ? '' : 'Verbrauchspreis NT: '.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
 $tgrund='Grundpreis: '.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').' EUR pro Jahr';
-$ttinhalt='<div class=\"bfToolTipLabel\"><b>Ihre Preisbestandteile ...</b><div/>'.$tarbeitspreis.$tarbeitspreisnt.$tgrund;
+/*$ttinhalt='<div class=\"bfToolTipLabel\"><b>Ihre Preisbestandteile ...</b><div/>'.$tarbeitspreis.$tarbeitspreisnt.$tgrund;
 echo '<div class="whTarifWrapper">'."\n";
-echo '<div class="whTarif">'."\n";
-echo '   <p class="whTitel">'.$tname.'</p>';
-echo '   <div id="whPreis" class="whPreis">'.$tpreis.'<br>'.$tpreisbeschr.'<br><span id="whPreisbestandteile'.$i.'" class="bfTooltip"></span></div>'."\n";
-echo '
-		<script type="text/javascript"><!--
-			JQuery(document).ready(function() {
-				JQuery("#whPreisbestandteile'.$i.'").qtip({
-					position: { adjust: { screen: true }},
-					content: "'.$ttinhalt.'",
-					style: {
-						tip: !JQuery.browser.ie,
-						background: "#ffc",
-						color: "#000000",
-						border : {color: "#C0C0C0", width: 1 },
-						name: "cream"
-					}
-				});
-			});
-		//--></script>';
-#echo '   <div class="whPreisbeschreibung">'.$tpreisbeschr.'</div>'."\n";
-#echo '   <div class="whSeparator"></div>';
-#echo '   <div class="whText">'.$tinfo.'</div>'."\n";
-echo '</div>'."\n";
+	echo '<div class="whTarif">'."\n";
+	echo '   <p class="whTitel">'.$tname.'</p>';
+	echo '   <div id="whPreis" class="whPreis">'.$tpreis.'<br>'.$tpreisbeschr.'<br><span id="whPreisbestandteile'.$i.'" class="bfTooltip"></span></div>'."\n";
+	echo '</div>'."\n";
 echo '   <div class="whRadio"><input '.($tchecked ? 'checked="checked" ' : '').' class="ff_elem" '.$tabIndex.$onclick.$onblur.$onchange.$onfocus.$onselect.$readonly.'type="radio" name="ff_nm_'.$mdata['bfName'].'[]" value="'.$tid.'" id="ff_elem'.$mdata['dbId'].$idExt.'"/>';
 echo '      '.$tname."</div>\n";
 echo '</div>'."\n";
+*/
+$modalParams = array();
+$modalParams['title']  = $tname;
+$modalParams['height'] = "400";
+$modalParams['width']  = "80%";
 
 ?>
+<div class="whTarifWrapper">
+	<a href="#modal<?php echo $tid?>" data-toggle="modal" role="button" class="btn btn-success">
+	<div class="whTarif">
+		<p class="whTitel"><?php echo $tname?></p>
+		<div class="whPreis"><?php echo $tpreis?></div>
+		<div class="whTarifInfo"></div>
+	</div>
+	</a>
+	<div class="whRadio">
+		<input <?php echo ($tchecked ? 'checked="checked" ' : '')?> class="ff_elem" <?php echo $tabIndex.$onclick.$onblur.$onchange.$onfocus.$onselect.$readonly ?>
+		type="radio" name="ff_nm_<?php echo $mdata['bfName']?>[]" value="<?php echo $tid?>" id="ff_elem<?php echo $mdata['dbId'].$idExt?>"/><?php echo $tname?>
+	</div>
+</div>
+<?php 
+$modalBody='
+		<div>
+			<p>'.$tinfo.'</p>
+		</div>
+';
+echo JHtml::_('bootstrap.renderModal', 'modal'.$tid , $modalParams, $modalBody);
+?>		
