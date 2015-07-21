0859ec5c67af7bbd2088ff65dd7192d0e8b4897b
tkempf <tkempf@hueper.de>
2015-07-21 14:30:40 +0200

    angeklickter Bömmel bekommt Klasse active

--- media/breezingforms/tarifrechner/tauber.php
+++ media/breezingforms/tarifrechner/tauber.php
@@ -8,18 +8,31 @@ $tid='tarif'.htmlentities(trim($iEx[2]), ENT_QUOTES, 'UTF-8');
 // $iEx[3] enthält	 den Selektor für das Template
 $tname=htmlentities(trim($iEx[4]), ENT_QUOTES, 'UTF-8');
 $tinfo='<h1>'.$tname.'</h1>'.trim($iEx[5]);
-$tarbeitspreis='Verbrauchspreis HT: '.htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
-$tarbeitspreisnt=($iEx[7] == 0) ? '' : 'Verbrauchspreis NT: '.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.<br>';
+$tarbeitspreis='Verbrauchspreis HT: '.htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.';
+$tarbeitspreisnt=($iEx[7] == 0) ? '' : 'Verbrauchspreis NT: '.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh.';
 $tgrund='Grundpreis: '.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').' EUR pro Jahr';
+$tpreise='<h3>'.$tgrund.'<br>'.$tarbeitspreis.'<br>'.$tarbeitspreisnt.'</h3>';
 $tinfobutton='<div class="whTarifFooter">
 				<button id="wechselbutton'.$tid.'" class="ff_elem bfCustomSubmitButton" onClick="JQuery(\'#ff_elem'.$mdata['dbId'].$idExt.'\').prop(\'checked\',true);ff_validate_nextpage(this,\'click\');">Jetzt wechseln</button>
 			  </div>';
 
 
-?>
+if(!$fdeclared){$fdeclared=true;?>
+<script type="text/javascript">
+<!--
+function wh_showTarifInfo(id){
+	   var myHTML=JQuery("#whTarifInfo" + id).html();
+	   JQuery("#tarifinfo").html(myHTML).show();
+	   JQuery(".whTarif").removeClass('active');
+	   JQuery("#whTarif" + id).addClass('active');
+	   //window.parent.JQuery(".breezingforms_iframe").iframeAutoHeight({heightOffset: 15, debug: false, diagnostics: false});
+}
+//-->
+</script>
+<?php }?>
 <div class="whTarifWrapper">
-	<div class="whTarifInfoHTML" id="whTarifInfo<?php echo $tid?>"><?php echo ($tinfo.$tinfobutton);?></div>	
-	<div class="whTarif" onClick="wh_showTarifInfo('<?php echo $tid?>')">
+	<div class="whTarifInfoHTML" id="whTarifInfo<?php echo $tid?>"><?php echo ($tinfo.$tpreise.$tinfobutton);?></div>	
+	<div class="whTarif" id="whTarif<?php echo $tid?>" onClick="wh_showTarifInfo('<?php echo $tid?>')">
 		<p class="whTitel"><?php echo $tname?></p>
 		<div class="whPreis"><?php echo $tpreis?><br><?php echo $tpreisbeschr?></div>
 		<div class="whRadio">
@@ -27,14 +40,3 @@ $tinfobutton='<div class="whTarifFooter">
 		</div>
 	</div>
 </div>
-<?php if(!$fdeclared){$fdeclared=true;?>
-<script type="text/javascript">
-<!--
-function wh_showTarifInfo(id){
-	   var myHTML=JQuery("#whTarifInfo" + id).html();
-	   JQuery("#tarifinfo").html(myHTML).show();
-	   //window.parent.JQuery(".breezingforms_iframe").iframeAutoHeight({heightOffset: 15, debug: false, diagnostics: false});
-}
-//-->
-</script>
-<?php }?>
