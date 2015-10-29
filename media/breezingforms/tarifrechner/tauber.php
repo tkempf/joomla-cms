06a566ac7c2b9f0efcee725bc269d229a396b5f4
tkempf <tkempf@hueper.de>
2015-10-29 14:28:22 +0100

    Die Preise im Tarifrechner nur 2 Nachkommastellen und mit Dezimaltabulator

--- media/breezingforms/tarifrechner/tauber.php
+++ media/breezingforms/tarifrechner/tauber.php
@@ -2,19 +2,19 @@
 // tarifradiodata Tauberenergie hat folgende Elemente
 // checked;gesamtpreis;tarifid;tauber;tarifanzeigename;tarifinfo;Endpreisbeschreibung;Arbeitspreis mit Beschreibung;ArbeitspreisNT mit Beschreibung;Grundpreis mit Beschreibung; 
 $tchecked=($iEx[0] == 1);
-$tarifart=$iEx[12];
+$tarifart=$iEx[13];
 $tpreis=htmlentities(trim($iEx[1]), ENT_QUOTES, 'UTF-8');
 $tpreisbeschr=' EUR/Jahr';
 $tid='tarif'.htmlentities(trim($iEx[2]), ENT_QUOTES, 'UTF-8');
 // $iEx[3] enthält	 den Selektor für das Template
 $tname=htmlentities(trim($iEx[4]), ENT_QUOTES, 'UTF-8');
 $tinfo='<h1>'.$tname.'</h1>'.trim($iEx[5]);
-$tarbeitspreis='<tr><td>Verbrauchspreis'.(($tarifart=='zweitarif') ? ' HT: ' : ':</td><td>').htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').' Cent pro kWh</td></tr>';
-$tarbeitspreisnt=($tarifart=='zweitarif') ? '<tr><td>Verbrauchspreis NT:</td><td>'.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').' Cent pro kWh</td></tr>' : '';
-$tgrund='<tr><td>Grundpreis:</td><td>'.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').' EUR pro Jahr</td><tr>';
-$tslp=($iEx[9]==0) ? '' : '<tr><td>Mehr-/Mindermengenpreis<br>Stand '.$iEx[10].':</td><td>'.$iEx[9].' Cent pro kWh<br>zzgl. Umsatzsteuer</td></tr>';
-$tbonus=($iEx[11]==0) ? '' : '<tr><td>'.htmlentities(trim($iEx[12]), ENT_QUOTES, 'UTF-8').':</td><td>'.htmlentities(trim($iEx[11]), ENT_QUOTES, 'UTF-8'). ' EUR</td></tr>';
-$tpreise='<div class="whPreisbestandteile"><table>'.$tgrund.$tarbeitspreis.$tarbeitspreisnt.$tslp.$tbonus.'</table></div>';
+$tarbeitspreis='<tr><td class="whPreistitel">Verbrauchspreis'.(($tarifart=='zweitarif') ? ' HT:</td><td class="whPreiszahl">' : ':</td><td>').htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').'</td><td>Cent pro kWh</td></tr>';
+$tarbeitspreisnt=($tarifart=='zweitarif') ? '<tr><td class="whPreistitel">Verbrauchspreis NT:</td><td class="whPreiszahl">'.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').'</td><td>Cent pro kWh</td></tr>' : '';
+$tgrund='<tr><td class="whPreistitel">Grundpreis:</td><td class="whPreiszahl">'.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').'</td><td>EUR pro Jahr</td><tr>';
+$tslp=($iEx[9]==0) ? '' : '<tr><td class="whPreistitel">Mehr-/Mindermengenpreis<br>Stand '.$iEx[10].':</td><td class="whPreiszahl">'.$iEx[9].'</td><td>Cent pro kWh</td></tr>';
+$tbonus=($iEx[11]==0) ? '' : '<tr><td class="whPreistitel">'.htmlentities(trim($iEx[12]), ENT_QUOTES, 'UTF-8').':</td><td class="whPreiszahl">'.htmlentities(trim($iEx[11]), ENT_QUOTES, 'UTF-8'). '</td><td>EUR</td></tr>';
+$tpreise='<div class="whPreisbestandteile"><table>'.$tgrund.$tarbeitspreis.$tarbeitspreisnt.$tslp.$tbonus.'</table><p>Alle Preise verstehen sich inkl. derzeit gültiger Umsatzsteuer.</p></div>';
 $tinfobutton='<div class="whTarifFooter">
 				<button id="wechselbutton'.$tid.'" class="ff_elem bfCustomSubmitButton" onClick="JQuery(\'#ff_elem'.$mdata['dbId'].$idExt.'\').prop(\'checked\',true);ff_validate_nextpage(this,\'click\');">Jetzt wechseln</button>
 			  </div>';
