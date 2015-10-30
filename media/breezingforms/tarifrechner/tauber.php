898d74b509e4b9eee6948ba1afd88ab574bb73bd
tkempf <tkempf@hueper.de>
2015-10-30 08:56:26 +0100

    Optische Änderungen auf Wunsch Herr Kuhn

--- media/breezingforms/tarifrechner/tauber.php
+++ media/breezingforms/tarifrechner/tauber.php
@@ -12,9 +12,9 @@ $tinfo='<h1>'.$tname.'</h1>'.trim($iEx[5]);
 $tarbeitspreis='<tr><td class="whPreistitel">Verbrauchspreis'.(($tarifart=='zweitarif') ? ' HT:</td><td class="whPreiszahl">' : ':</td><td>').htmlentities(trim($iEx[6]), ENT_QUOTES, 'UTF-8').'</td><td>Cent pro kWh</td></tr>';
 $tarbeitspreisnt=($tarifart=='zweitarif') ? '<tr><td class="whPreistitel">Verbrauchspreis NT:</td><td class="whPreiszahl">'.htmlentities(trim($iEx[7]), ENT_QUOTES, 'UTF-8').'</td><td>Cent pro kWh</td></tr>' : '';
 $tgrund='<tr><td class="whPreistitel">Grundpreis:</td><td class="whPreiszahl">'.htmlentities(trim($iEx[8]), ENT_QUOTES, 'UTF-8').'</td><td>EUR pro Jahr</td><tr>';
-$tslp=($iEx[9]==0) ? '' : '<tr><td class="whPreistitel">Mehr-/Mindermengenpreis<br>Stand '.$iEx[10].':</td><td class="whPreiszahl">'.$iEx[9].'</td><td>Cent pro kWh</td></tr>';
+$tslp=($iEx[9]==0) ? '' : '<tr><td class="whPreistitel">Mehr-/Mindermengenpreis<br>Stand '.$iEx[10].':</td><td class="whPreiszahl">&nbsp;<br>'.$iEx[9].'</td><td>&nbsp;<br>Cent pro kWh</td></tr>';
 $tbonus=($iEx[11]==0) ? '' : '<tr><td class="whPreistitel">'.htmlentities(trim($iEx[12]), ENT_QUOTES, 'UTF-8').':</td><td class="whPreiszahl">'.htmlentities(trim($iEx[11]), ENT_QUOTES, 'UTF-8'). '</td><td>EUR</td></tr>';
-$tpreise='<div class="whPreisbestandteile"><table>'.$tgrund.$tarbeitspreis.$tarbeitspreisnt.$tslp.$tbonus.'</table><p>Alle Preise verstehen sich inkl. derzeit gültiger Umsatzsteuer.</p></div>';
+$tpreise='<div class="whPreisbestandteile"><p>Ihr Verbrauchspreis setzt sich wie folgt zusammen:</p><table>'.$tgrund.$tarbeitspreis.$tarbeitspreisnt.$tslp.$tbonus.'</table><p>Alle Preise verstehen sich inkl. derzeit gültiger Umsatzsteuer.</p></div>';
 $tinfobutton='<div class="whTarifFooter">
 				<button id="wechselbutton'.$tid.'" class="ff_elem bfCustomSubmitButton" onClick="JQuery(\'#ff_elem'.$mdata['dbId'].$idExt.'\').prop(\'checked\',true);ff_validate_nextpage(this,\'click\');">Jetzt wechseln</button>
 			  </div>';
